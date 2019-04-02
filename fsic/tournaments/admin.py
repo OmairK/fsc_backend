b59f57ae441927252f3a231906a3216c45534d92
Daniyal <abbasi.daniyal98@gmail.com>
2019-04-03 01:55:14 +0530

    Update Admin site

--- fsic/tournaments/admin.py
+++ fsic/tournaments/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import TournamentFSC,TournamentITF
+
+
+
+
+
+admin.site.register(TournamentFSC)
+admin.site.register(TournamentITF)
