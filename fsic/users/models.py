78ebd60b7e49dd31b40091f8f8d49c4c73933381
Omair <omair@khan.com>
2019-04-21 02:21:51 +0530

    Update the validator to 2Mb

--- fsic/users/models.py
+++ fsic/users/models.py
@@ -10,7 +10,7 @@ def validate_file_size(value):
     filesize= value.size
     
     if filesize > 2*1024*1024:
-        raise ValidationError("The maximum file size that can be uploaded is 10MB")
+        raise ValidationError("The maximum file size that can be uploaded is 2MB")
     else:
         return value
 class UserProfile(models.Model):
